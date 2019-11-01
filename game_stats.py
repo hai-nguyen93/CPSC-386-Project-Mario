9cbed08f3e5502de17a2bc3302434befccd98ab4
Hai Nguyen <54726078+hai-nguyen93@users.noreply.github.com>
2019-10-31 17:03:09 -0700

    mario

--- game_stats.py
+++ game_stats.py
@@ -1,7 +1,7 @@
 class GameStats:
     def __init__(self):
         self.game_status = 0  # 0: menu, 1: game active
-        self.current_stage = 8
+        self.current_stage = 1
         self.swim_stage = 6
         self.credits_stage = 9
         self.score = 0
